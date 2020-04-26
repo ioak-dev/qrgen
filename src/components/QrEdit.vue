935179e60ffd4b60b0822e9f357834e9b9157367
rousseaumm <rousseau.mm1988@gmail.com>
2020-04-26 14:10:45 -0400

    Added fields for VCard and MeCard

--- src/components/QrEdit.vue
+++ src/components/QrEdit.vue
@@ -34,6 +34,289 @@
           @change="handleChange('wifi')"
         />
       </div>
+      <div v-if="data.contentType === 'contactMe'">
+        <oak-text
+          id="adr"
+          label="Address"
+          :data="data.contactMe.adr"
+          @change="handleChange('contactMe')"
+        />
+        <oak-text
+          id="bday"
+          label="BirthDate(yyyymmdd)"
+          :data="data.contactMe.bday"
+          @change="handleChange('contactMe')"
+        />
+        <oak-text
+          id="email"
+          label="Email"
+          :data="data.contactMe.email"
+          @change="handleChange('contactMe')"
+        />
+        <oak-text
+          id="n"
+          label="Name(Last, First)"
+          :data="data.contactMe.n"
+          @change="handleChange('contactMe')"
+        />
+        <oak-text
+          id="nickname"
+          label="Nick Name"
+          :data="data.contactMe.nickname"
+          @change="handleChange('contactMe')"
+        />
+        <oak-text
+          id="note"
+          label="Note"
+          :data="data.contactMe.note"
+          @change="handleChange('contactMe')"
+        />
+        <oak-text
+          id="sound"
+          label="Sound"
+          :data="data.contactMe.sound"
+          @change="handleChange('contactMe')"
+        />
+        <oak-text
+          id="tel"
+          label="Telephone"
+          :data="data.contactMe.tel"
+          @change="handleChange('contactMe')"
+        />
+        <oak-text
+          id="telav"
+          label="Tel-AV"
+          :data="data.contactMe.telav"
+          @change="handleChange('contactMe')"
+        />
+        <oak-text
+          id="url"
+          label="Website"
+          :data="data.contactMe.url"
+          @change="handleChange('contactMe')"
+        />
+      </div>
+      <div v-if="data.contentType === 'contactV'">
+        <oak-text
+          id="addr"
+          label="Address"
+          :data="data.contactV.adr"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="addr"
+          label="Address"
+          :data="data.contactV.adr"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="anniversary"
+          label="Anniversary(yyyymmdd)"
+          :data="data.contactV.anniversary"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="bday"
+          label="BirthDay(yyyymmdd)"
+          :data="data.contactV.bday"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="begin"
+          label="Begin"
+          :data="data.contactV.begin"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="caladruri"
+          label="Caladruri"
+          :data="data.contactV.caladruri"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="caluri"
+          label="Caluri"
+          :data="data.contactV.caluri"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="categories"
+          label="Categories"
+          :data="data.contactV.categories"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="clientpidmap"
+          label="ClientPidMap"
+          :data="data.contactV.clientpidmap"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="email"
+          label="Email"
+          :data="data.contactV.email"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="fburl"
+          label="FBURL"
+          :data="data.contactV.fburl"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="fn"
+          label="Formatted Name"
+          :data="data.contactV.fn"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="gender"
+          label="Gender"
+          :data="data.contactV.gender"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="geo"
+          label="GEO"
+          :data="data.contactV.geo"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="impp"
+          label="Instant Messenger Handle"
+          :data="data.contactV.impp"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="key"
+          label="Public Encryption Key"
+          :data="data.contactV.key"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="kind"
+          label="Kind"
+          :data="data.contactV.impp"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="lang"
+          label="Language"
+          :data="data.contactV.lang"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="logo"
+          label="Logo"
+          :data="data.contactV.logo"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="member"
+          label="Member"
+          :data="data.contactV.member"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="n"
+          label="Name"
+          :data="data.contactV.n"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="nickname"
+          label="Nickname"
+          :data="data.contactV.nickname"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="note"
+          label="Note"
+          :data="data.contactV.note"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="org"
+          label="Org"
+          :data="data.contactV.org"
+          @change="handleChange('contactV')"
+        />
+        <div class="file">
+          <form enctype=""></form>
+        </div>
+        <oak-text
+          id="prodid"
+          label="Product Identifier"
+          :data="data.contactV.prodid"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="related"
+          label="Related"
+          :data="data.contactV.related"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="rev"
+          label="Rev"
+          :data="data.contactV.rev"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="role"
+          label="Role"
+          :data="data.contactV.role"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="sound"
+          label="Sound"
+          :data="data.contactV.sound"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="source"
+          label="Source"
+          :data="data.contactV.source"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="tel"
+          label="Telephone"
+          :data="data.contactV.tel"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="title"
+          label="Title"
+          :data="data.contactV.title"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="tz"
+          label="Time Zone"
+          :data="data.contactV.tz"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="uid"
+          label="UID"
+          :data="data.contactV.uid"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="url"
+          label="Website"
+          :data="data.contactV.url"
+          @change="handleChange('contactV')"
+        />
+        <oak-text
+          id="xml"
+          label="XML"
+          :data="data.contactV.xml"
+          @change="handleChange('contactV')"
+        />
+      </div>
       <div v-if="data.contentType === 'plaintext'">
         <oak-text
           id="text"
@@ -108,6 +391,59 @@ export default {
         },
         url: {
           text: '',
+        },
+        contactMe: {
+          adr: '',
+          bday: '',
+          email: '',
+          n: '',
+          nickname: '',
+          note: '',
+          sound: '',
+          tel: '',
+          telav: '',
+          url: '',
+        },
+        contactV: {
+          adr: '',
+          anniversary: '',
+          bday: '',
+          begin: '',
+          caladruri: '',
+          caluri: '',
+          categories: '',
+          clientpidmap: '',
+          email: '',
+          end: '',
+          fburl: '',
+          fn: '',
+          gender: '',
+          geo: '',
+          impp: '',
+          key: '',
+          kind: '',
+          optional: '',
+          lang: '',
+          logo: '',
+          member: '',
+          n: '',
+          name: '',
+          nickname: '',
+          note: '',
+          org: '',
+          prodid: '',
+          related: '',
+          rev: '',
+          role: '',
+          sound: '',
+          soource: '',
+          tel: '',
+          title: '',
+          tz: '',
+          uid: '',
+          url: '',
+          version: '',
+          xml: '',
         },
       },
       supported: {
